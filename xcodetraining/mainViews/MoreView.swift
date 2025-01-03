f99e0d1fa0ec74fb1a7d3be5e0a11cf9e32896e5
Mamodesaebsaad <saadmamodesaeb@gmail.com>
2025-01-03 17:59:52 +0400

    add More view

---
+++ xcodetraining/mainViews/MoreView.swift
@@ -0,0 +1,18 @@
+//
+//  MoreView.swift
+//  xcodetraining
+//
+//  Created by saad Mamode saeb on 03/01/2025.
+//
+
+import SwiftUI
+
+struct MoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoreView()
+}
