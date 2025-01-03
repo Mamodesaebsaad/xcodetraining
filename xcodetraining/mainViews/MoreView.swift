99504471f29d669216727510af989e56ea44fe42
Mamodesaebsaad <saadmamodesaeb@gmail.com>
2025-01-03 18:00:24 +0400

    more view

--- xcodetraining/mainViews/MoreView.swift
+++ xcodetraining/mainViews/MoreView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct MoreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                Text("Hello, World!")
+            }
+        }
     }
 }
 
